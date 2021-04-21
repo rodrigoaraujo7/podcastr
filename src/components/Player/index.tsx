3f7393a0922e770295e633a1d0d7ce333e172bf7
rodrigoaraujo7 <rodrigoleitearaujo777@gmail.com>
2021-04-20 23:57:58 -0300

    Player Buttons

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -12,8 +12,34 @@ export default function Player() {
                 <strong>Selecione um podcast para ouvir</strong>
             </div>
 
-            <footer>
+            <footer className={styles.empty}>
+                <div className={styles.progress}>
+                    <span>00:00</span>
 
+                    <div className={styles.slider}>
+                        <div className={styles.emptySlider} />
+                    </div>
+
+                    <span>00:00</span>
+                </div>
+
+                <div className={styles.buttons}>
+                    <button type="button">
+                        <img src="/shuffle.svg" alt="Shuffle" />
+                    </button>
+                    <button type="button">
+                        <img src="/play-previous.svg" alt="Previous" />
+                    </button>
+                    <button type="button" className={styles.playButton}>
+                        <img src="/play.svg" alt="Play" />
+                    </button>
+                    <button type="button">
+                        <img src="/play-next.svg" alt="Next" />
+                    </button>
+                    <button type="button">
+                        <img src="/repeat.svg" alt="Repeat" />
+                    </button>
+                </div>
             </footer>
         </div>
     );
