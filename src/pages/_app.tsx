f16fe0bcb696789d94c0c9cc197cda99cbdd11eb
rodrigoaraujo7 <rodrigoleitearaujo777@gmail.com>
2021-04-20 23:16:50 -0300

    Player Container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,20 @@
+import styles from '../styles/app.module.scss';
+
+import Header from '../components/Header';
+import Player from '../components/Player';
+
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  )
 }
 
 export default MyApp
