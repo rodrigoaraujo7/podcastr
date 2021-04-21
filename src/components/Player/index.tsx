e7e737dbd8fe1d68e390d9e1286e4ee07bed89d5
rodrigoaraujo7 <rodrigoleitearaujo777@gmail.com>
2021-04-20 23:24:32 -0300

    Empty Player

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -3,7 +3,18 @@ import styles from './styles.module.scss';
 export default function Player() {
     return (
         <div className={styles.playerContainer}>
+            <header>
+                <img src="/playing.svg" alt="Playing Now" />
+                <strong>Tocando Agora</strong>
+            </header>
 
+            <div className={styles.emptyPlayer}>
+                <strong>Selecione um podcast para ouvir</strong>
+            </div>
+
+            <footer>
+
+            </footer>
         </div>
     );
 }
