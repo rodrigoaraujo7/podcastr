fdd8d5f578875d416049bf8d926d7d7dad9f06b1
rodrigoaraujo7 <rodrigoleitearaujo777@gmail.com>
2021-04-22 13:05:00 -0300

    Back to home Button

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -2,6 +2,7 @@ import { format, parseISO } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
+import Link from "next/link";
 
 import { api } from "../../services/api";
 import { convertDurationToTimeString } from "../../utils/convertDurationToTimeString";
@@ -28,9 +29,11 @@ export default function Episode({ episode }: EpisodeProps) {
   return (
     <div className={styles.episode}>
       <div className={styles.thumbnailContainer}>
-        <button type="button">
-          <img src="/arrow-left.svg" alt="Back" />
-        </button>
+        <Link href="/">
+            <button type="button">
+                <img src="/arrow-left.svg" alt="Back" />
+            </button>
+        </Link>
         <Image
           width={700}
           height={160}
