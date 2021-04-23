746990cc863ae5daa1c6b4275fe3d659decf1786
rodrigoaraujo7 <rodrigoleitearaujo777@gmail.com>
2021-04-23 00:15:53 -0300

    Play and Pause Butotn

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,13 +1,31 @@
 import Image from "next/image";
-import { useContext } from "react";
+import { useContext, useRef, useEffect } from "react";
 import { PlayerContext } from "../../contexts/PlayerContext";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import styles from "./styles.module.scss";
 
-
 export default function Player() {
-  const { episodeList, currentEpisodeIndex } = useContext(PlayerContext);
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const {
+    episodeList,
+    currentEpisodeIndex,
+    isPlaying,
+    togglePlay,
+  } = useContext(PlayerContext);
+
+  useEffect(() => {
+      if (!audioRef.current) {
+          return;
+      } 
+
+      if (isPlaying) {
+          audioRef.current.play();
+      } else {
+          audioRef.current.pause();
+      }
+  }, [isPlaying])
 
   const episode = episodeList[currentEpisodeIndex];
 
@@ -20,14 +38,14 @@ export default function Player() {
 
       {episode ? (
         <div className={styles.currentEpisode}>
-            <Image 
-                width={592} 
-                height={592} 
-                src={episode.thumbnail}
-                objectFit="cover"
-            />
-            <strong>{episode.title}</strong>
-            <span>{episode.members}</span>
+          <Image
+            width={592}
+            height={592}
+            src={episode.thumbnail}
+            objectFit="cover"
+          />
+          <strong>{episode.title}</strong>
+          <span>{episode.members}</span>
         </div>
       ) : (
         <div className={styles.emptyPlayer}>
@@ -35,39 +53,55 @@ export default function Player() {
         </div>
       )}
 
-      <footer className={!episode ? styles.empty : ''}>
+      <footer className={!episode ? styles.empty : ""}>
         <div className={styles.progress}>
           <span>00:00</span>
 
           <div className={styles.slider}>
-            { episode ? (
-                <Slider 
-                    trackStyle={{ backgroundColor: '#04d361' }}
-                    railStyle={{ backgroundColor: '#9f75ff' }}
-                    handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}
-                />
+            {episode ? (
+              <Slider
+                trackStyle={{ backgroundColor: "#04d361" }}
+                railStyle={{ backgroundColor: "#9f75ff" }}
+                handleStyle={{ borderColor: "#04d361", borderWidth: 4 }}
+              />
             ) : (
-                <div className={styles.emptySlider} />
+              <div className={styles.emptySlider} />
             )}
           </div>
 
           <span>00:00</span>
         </div>
 
+        {episode && 
+            <audio 
+            src={episode.url} 
+            ref={audioRef}
+            autoPlay 
+        />}
+
         <div className={styles.buttons}>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Shuffle" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-previous.svg" alt="Previous" />
           </button>
-          <button type="button" className={styles.playButton}>
-            <img src="/play.svg" alt="Play" />
+          <button
+            type="button"
+            className={styles.playButton}
+            disabled={!episode}
+            onClick={togglePlay}
+          >
+            {isPlaying ? (
+              <img src="/pause.svg" alt="Pause" />
+            ) : (
+              <img src="/play.svg" alt="Play" />
+            )}
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-next.svg" alt="Next" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/repeat.svg" alt="Repeat" />
           </button>
         </div>
--- src/contexts/PlayerContext.ts
+++ src/contexts/PlayerContext.ts
@@ -11,7 +11,9 @@ type Episode = {
 type PlayerContextData = {
     episodeList: Episode[];
     currentEpisodeIndex: number;
+    isPlaying: boolean;
     play: (episode: Episode) => void;
+    togglePlay: () => void;
 };
 
 export const PlayerContext = createContext({} as PlayerContextData);
